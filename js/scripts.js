62865ab31883178763a3fec0b8e590aceb1d3750
The Geek Viking <christian.sergerie@gmail.com>
2022-05-15 07:35:18 -0400

    Initial Commit

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,37 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+// owl carousel
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        nav:true,
+        dots:true,
+        autoplay:true,
+        autoplaySpeed:1000,
+        smartSpeed:1500,
+        autoplayHoverPause:true
+    });
+});
+
+//VLOG
+
+let listVideo = document.querySelectorAll('.video-list .vid');
+let mainVideo = document.querySelector('.main-video video');
+let title = document.querySelector('.main-video .title');
+
+listVideo.forEach(video => {
+    video.onclick = () => {
+        listVideo.forEach(vid => vid.classList.remove('active'));
+        video.classList.add('active');
+        if(video.classList.contains('active')) {
+            let src = video.children[0].getAttribute('src');
+            mainVideo.src = src;
+            let text = video.children[1].innerHTML;
+            title.innerHTML = text;
+        };
+    };
+});
