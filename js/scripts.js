4878f73f4d98e3487a4934f44fff32516c858fcb
The Geek Viking <christian.sergerie@gmail.com>
2022-05-30 14:36:40 -0400

    Initial Commit

--- js/scripts.js
+++ js/scripts.js
@@ -86,3 +86,32 @@ listVideo.forEach(video => {
         };
     };
 });
+
+// Modals
+
+// Get DOM Elements
+const modal = document.querySelector('#my-modal-services');
+const modalBtn = document.querySelector('#modal-btn-services');
+const closeBtn = document.querySelector('.close-services');
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
